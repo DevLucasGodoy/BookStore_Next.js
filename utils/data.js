6194025384e8459a8debeee9d80e1720bb879aad
LGSTWebSites <lucas.moraes.godoy@gmail.com>
2022-04-25 22:53:46 -0300

    Update data.js

--- utils/data.js
+++ utils/data.js
@@ -2,6 +2,7 @@ const data = {
   products: [
     {
       name: 'Cabeça Fria, Coração Quente',
+      slug: 'cabeca-fria-coracao-quente',
       category: 'Shirts',
       image: '/images/imagem01.jpg',
       price: 1000,
@@ -13,6 +14,7 @@ const data = {
     },
     {
       name: 'Imponente - a Rec. Do Palmeiras, da Ar. Ao Decat',
+      slug: 'imponente',
       category: 'Shirts',
       image: '/images/imagem02.jpg',
       price: 80,
@@ -24,6 +26,7 @@ const data = {
     },
     {
       name: 'Guardiola Confidencial: Um Ano Dentro Do Bayern De Munique Acompanhando De Perto O Técnico Que Mudou O Futebol Para Sempre',
+      slug: 'guardiola-confidencial',
       category: 'Shirts',
       image: '/images/imagem03.jpg',
       price: 90,
@@ -35,6 +38,7 @@ const data = {
     },
     {
       name: 'A pirâmide invertida: A história da tática no futebol',
+      slug: 'piramede-invertida',
       category: 'Pants',
       image: '/images/imagem04.jpg',
       price: 90,
@@ -46,6 +50,7 @@ const data = {
     },
     {
       name: 'Mourinho Rockstar: As Diversas Faces Do Técnico Mais Polêmico Do Mundo',
+      slug: 'mourinho-rockstar',
       category: 'Pants',
       image: '/images/imagem05.jpg',
       price: 95,
@@ -57,6 +62,7 @@ const data = {
     },
     {
       name: 'Escola brasileira de futebol',
+      slug: 'escola-brasileira-de-futebol',
       category: 'Pants',
       image: '/images/imagem06.jpg',
       price: 75,
