95d0c9a6619650133d8f0675380b43437f2b04e3
LGSTWebSites <lucas.moraes.godoy@gmail.com>
2022-04-25 22:26:41 -0300

    new home page

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,45 @@
-import Head from 'next/head';
-import Image from 'next/image';
+import {
+  Grid,
+  Card,
+  CardActionArea,
+  CardMedia,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+} from '@material-ui/core';
 import Layout from '../components/Layout';
-import styles from '../styles/Home.module.css';
+import data from '../utils/data';
 
 export default function Home() {
   return (
     <Layout>
       <div>
-        <h1>Livos em Destaque</h1>
-        <ul>
-          <li>Livro 01</li>
-          <li>Livro 02</li>
-          <li>Livro 03</li>
-          <li>Livro 04</li>
-          <li>Livro 05</li>
-          <li>Livro 06</li>
-        </ul>
+        <h1>Livros em Destaque</h1>
+        <Grid container spacing={3}>
+          {data.products.map((product) => (
+            <Grid item md={4} key={product.name}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    image={product.image}
+                    title={product.name}
+                  ></CardMedia>
+                  <CardContent>
+                    <Typography>{product.name}</Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Typography>${product.price}</Typography>
+                  <Button size="small" color="primary">
+                    Add to cart
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
       </div>
     </Layout>
   );
