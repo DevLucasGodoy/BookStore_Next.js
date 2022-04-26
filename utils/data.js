533d0f330c0ec5f03321934ead17ee852cc60949
LGSTWebSites <lucas.moraes.godoy@gmail.com>
2022-04-25 23:52:53 -0300

    Update data.js

--- utils/data.js
+++ utils/data.js
@@ -20,11 +20,12 @@ const data = {
       category: 'Esportes',
       image: '/images/imagem02.jpg',
       price: 80,
-      brand: 'Adidas',
+      brand: 'Celso De Campos Jr. (Autor), cesar Greco (Autor)',
       rating: 4.2,
       numReviews: 10,
       countInStock: 20,
-      description: 'Imponente',
+      description:
+        'A chegada do allianz parque foi o divisor de águas de uma das maiores reconstruções da história do palmeiras. Aproveitando todo o potencial da nova arena, o clube, norteado pelo signo do profissionalismo, canalizou o apoio de parceiros e torcedores para montar, de novo, esquadrões à altura de sua tradição – que conquistaram três campeonatos nacionais nos quatro anos seguintes e devolveram o palestra ao topo do futebol brasileiro. No livro “imponente - a reconstrução do palmeiras, da arena ao deca”, o jornalista celso de campos jr. E o fotógrafo cesar greco nos conduzem pelos melhores momentos dessa memorável jornada, que redefiniu por completo o destino do alviverde. De novo, imponente.',
     },
     {
       name: 'Guardiola Confidencial: Um Ano Dentro Do Bayern De Munique Acompanhando De Perto O Técnico Que Mudou O Futebol Para Sempre',
@@ -32,11 +33,13 @@ const data = {
       category: 'Esportes',
       image: '/images/imagem03.jpg',
       price: 90,
-      brand: 'Raymond',
+      brand:
+        'Martí Perarnau  (Autor), Gabriel Roberti Gobeth (Tradutor), André Kfouri (Prefácio), Aline Temoteo (Arte de Capa).',
       rating: 4.5,
       numReviews: 10,
       countInStock: 20,
-      description: 'Guardiola Confidencial',
+      description:
+        'Josep Guadiola i Sala, ou simplesmente Pep Guardiola, o nome que o futebol consagrou, é o personagem central do livro. Artífice ideológico, como treinador, de um tipo de jogo plástico, vencedor e revolucionário, Pep deixou o Barcelona, onde conquistara todos os títulos possíves, para se lançar em um novo projeto profissional no Bayern de Munique, outro gigante do futebol europeu. E para escrever sobre ele, o jornalista Martí Perarnau acompanhou durante um ano o dia a dia do novo comandante do Bayern. Perarnau obteve do próprio Guardiola permissão para compartilhar o vestiário com os atletas, seguir de perto todos os passos do técnico e detalhar uma temporada inteira do catalão no comando do clube. Estava lançado o desafio a Martí: decifrar a personalidade de Pep Guardiola, o técnico que deu ao Barcelona os melhores anos de sua história. O autor soube aproveitar o acesso livre – sem precedentes! – aos bastidores de um dos maiores clubes de futebol do mundo. Observou de perto um personagem apaixonado pelo futebol, meticuloso e obsessivo em sua busca pela perfeição. Pôde conhecer melhor também as ideias e conceitos de jogo fundamentais para o técnico, desfazendo ao longo da obra uma série de clichês que rodeiam a figura de Guardiola. Em artigo escrito para o jornal The Guardian, Perarnau descreve a personalidade de um técnico ao mesmo tempo racional e extremamente emocional.',
     },
     {
       name: 'A pirâmide invertida: A história da tática no futebol',
@@ -44,11 +47,13 @@ const data = {
       category: 'Esportes',
       image: '/images/imagem04.jpg',
       price: 90,
-      brand: 'Oliver',
+      brand:
+        ' Jonathan Wilson  (Autor), André Kfouri (Tradutor), Tupiniquim Design (Estilista), Aline Temoteo (Arte de Capa).',
       rating: 4.5,
       numReviews: 10,
       countInStock: 20,
-      description: 'A pirâmide invertida',
+      description:
+        'Prepare-se para conhecer a “bíblia da tática”. Em A pirâmide invertida, o britânico Jonathan Wilson refaz todo o roteiro histórico da tática no futebol. Desde as origens do jogo, quando o caos ainda reinava, seus relatos percorrem a já longínqua era dos cinco atacantes, passam pelas grandes transformações da segunda metade do século XX e chegam enfim aos tempos modernos, em que a “pirâmide” se inverteu completamente para consagrar os esquemas com apenas um homem na linha de frente. A obra, que é uma referência internacional entre os livros dedicados ao futebol, vem cumprindo nos países onde já foi publicada um papel fascinante: o de induzir uma mudança gradual no comportamento dos apaixonados pelo esporte. Temas como a observação dos aspectos táticos do jogo, o entendimento dos movimentos dos atletas em campo e o gosto pela história do futebol passaram por um verdadeiro reposicionamento de expectativas: de tópicos pouco conhecidos, nem sempre bem-vindos nas conversas do dia a dia ou nos bate-papos pela televisão, transformaram-se em favoritos de crítica e público. E a razão para isso é simples: o livro toma o leitor pelo braço em um passeio pela história do esporte das massas, registrando ao longo do caminho cada etapa da evolução do jogo e revelando em detalhes os traços de estilo e as ideias inovadoras dos personagens mais importantes de todo esse processo.',
     },
     {
       name: 'Mourinho Rockstar: As Diversas Faces Do Técnico Mais Polêmico Do Mundo',
@@ -56,11 +61,12 @@ const data = {
       category: 'Esportes',
       image: '/images/imagem05.jpg',
       price: 95,
-      brand: 'Zara',
+      brand: 'Luís Aguilar (Autor), Paulo Vinicius Coelho (Prefácio).',
       rating: 4.5,
       numReviews: 10,
       countInStock: 20,
-      description: 'Mourinho Rockstar',
+      description:
+        'José Mário dos Santos Mourinho Félix é seu nome de batismo. Para o futebol, trata-se apenas de José Mourinho, um dos técnicos mais vencedores da história do esporte – e um especialista em polêmicas. Os títulos alcançados ao longo de uma carreira construída em gigantes do futebol europeu atestam a competência do treinador português, que aos poucos ganhou também a aura de mestre do pragmatismo. Mas Mourinho não é lembrado apenas pelos títulos, muito pelo contrário: é dono de uma das personalidades mais intrigantes do mundo do esporte, capaz de despertar sentimentos opostos, e sempre intensos. Mourinho Rockstar, é uma coleção de grandes momentos da vida do técnico, que não deixa também de lançar luzes sobre seus maiores fracassos. Constrói-se apoiado em polêmicas, desavenças, grandes conquistas e episódios inesquecíveis para combinar os altos e baixos no temperamento do português com a sina de vitórias que parece sempre acompanhá-lo. Afinal, Mourinho pode ser o rebelde estampado na capa da revista Rolling Stone, um espírito rock’n’roll que invadiu o mundo do futebol, mas é também, e inegavelmente, um anti-herói muito bem-sucedido. Entre os personagens retratados no livro, encontram-se os grandes protagonistas do futebol mundial: Guardiola, Ibrahimovic, sir Alex Ferguson, Balotelli, Arrigo Sacchi, Cristiano Ronaldo.',
     },
     {
       name: 'Escola brasileira de futebol',
@@ -68,11 +74,12 @@ const data = {
       category: 'Esportes',
       image: '/images/imagem06.jpg',
       price: 75,
-      brand: 'Casely',
+      brand: 'Paulo Vinícius Coelho (PVC)',
       rating: 4.5,
       numReviews: 10,
       countInStock: 20,
-      description: 'Escola brasileira de futebol',
+      description:
+        'O jornalista esportivo mais original de todos os tempos em um livro que destrincha, de forma inédita, o estilo brasileiro de jogar bola. Por que o Brasil forma tantos jogadores? O futebol brasileiro é conhecido e admirado no mundo inteiro, mas, por algum motivo, a escola brasileira de futebol nunca foi registrada em nenhum livro. Paulo Vinícius Coelho, o PVC, analisa os diversos esquemas táticos utilizados por times consagrados e pela vitoriosa seleção brasileira, buscando entender como o futebol brasileiro evoluiu ao longo de décadas. A escola brasileira de futebol é uma mescla de estilos, alimentada por técnicos que fizeram história e que formaram jogadores históricos. O Brasil é, sem dúvida, o país do futebol, e por isso a sua escola precisa ser difundida, tanto pela bola nos pés quanto pelo conhecimento sobre sua teoria.',
     },
   ],
 };
