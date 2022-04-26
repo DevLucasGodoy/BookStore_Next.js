7a954f7e4d80420e7aa6e4174ca7c33866253c06
LGSTWebSites <lucas.moraes.godoy@gmail.com>
2022-04-25 22:06:16 -0300

    styles in home page

--- components/Layout.js
+++ components/Layout.js
@@ -1,21 +1,27 @@
-import React, { Children } from 'react';
+import React, { children } from 'react';
 import Head from 'next/head';
 import { AppBar, Container, Toolbar, Typography } from '@material-ui/core';
+import useStyles from '../utils/styles';
 
 export default function Layout({ children }) {
+  const classes = useStyles();
   return (
     <div>
       <Head>
         <title>LucasGodoy</title>
       </Head>
 
-      <AppBar position="static">
+      <AppBar position="static" className={classes.navbar}>
         <Toolbar>
           <Typography>Livraria do Godoy</Typography>
         </Toolbar>
       </AppBar>
 
-      <Container>{children}</Container>
+      <Container className={classes.main}>{children}</Container>
+
+      <footer className={classes.footer}>
+        <Typography>All rights reserved. Lucas Godoy @2022</Typography>
+      </footer>
     </div>
   );
 }
