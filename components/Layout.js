3ddf3ce9a985fb0b173c8afa6a1548e7fd69463a
LGSTWebSites <lucas.moraes.godoy@gmail.com>
2022-04-25 23:31:30 -0300

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -7,10 +7,37 @@ import {
   Typography,
   Container,
   Link,
+  createMuiTheme,
+  ThemeProvider,
+  CssBaseline,
 } from '@material-ui/core';
 import useStyles from '../utils/styles';
 
 export default function Layout({ title, description, children }) {
+  const theme = createMuiTheme({
+    typography: {
+      h1: {
+        fontSize: '1.6rem',
+        fontWeight: 400,
+        margin: '1rem 0',
+      },
+      h2: {
+        fontSize: '1.4rem',
+        fontWeight: 400,
+        margin: '1rem 0',
+      },
+    },
+    palette: {
+      type: 'light',
+      primary: {
+        main: '#f0c000',
+      },
+      secondary: {
+        main: '#208080',
+      },
+    },
+  });
+
   const classes = useStyles();
   return (
     <div>
@@ -19,32 +46,33 @@ export default function Layout({ title, description, children }) {
         {description && <meta name="description" content={description}></meta>}
       </Head>
 
-      <AppBar position="static" className={classes.navbar}>
-        <Toolbar>
-          <NextLink href="/" passHref>
-            <Link>
-              <Typography className={classes.brand}>
-                Livraria do Godoy
-              </Typography>
-            </Link>
-          </NextLink>
-          <div className={classes.grow}></div>
-          <div>
-            <NextLink href="/cart" passHref>
-              <Link>Carrinho</Link>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="static" className={classes.navbar}>
+          <Toolbar>
+            <NextLink href="/" passHref>
+              <Link>
+                <Typography className={classes.brand}>
+                  Livraria do Godoy
+                </Typography>
+              </Link>
             </NextLink>
-            <NextLink href="/login" passHref>
-              <Link>Login</Link>
-            </NextLink>
-          </div>
-        </Toolbar>
-      </AppBar>
-
-      <Container className={classes.main}>{children}</Container>
-
-      <footer className={classes.footer}>
-        <Typography>All rights reserved. Lucas Godoy @2022</Typography>
-      </footer>
+            <div className={classes.grow}></div>
+            <div>
+              <NextLink href="/cart" passHref>
+                <Link>Carrinho</Link>
+              </NextLink>
+              <NextLink href="/login" passHref>
+                <Link>Login</Link>
+              </NextLink>
+            </div>
+          </Toolbar>
+        </AppBar>
+        <Container className={classes.main}>{children}</Container>
+        <footer className={classes.footer}>
+          <Typography>All rights reserved. @LucasGodoy </Typography>
+        </footer>
+      </ThemeProvider>
     </div>
   );
 }
